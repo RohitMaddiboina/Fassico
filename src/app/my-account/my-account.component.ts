4b25fb72eb55889144fef593d6a3b5888b9a9071
Rohityadav19 <rohitteja19@gmail.com>
2021-10-31 23:28:40 +0530

    added order history in Profile page

--- src/app/my-account/my-account.component.ts
+++ src/app/my-account/my-account.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Orders } from '../models/Order.model';
 import { User } from '../models/user.model'; 
+import { CartService } from '../service/cartService/cart.service';
 import { CheckAuthService } from '../service/checkAuthService/check-auth.service';
+import { OrdersService } from '../service/orders/orders.service';
 import { RestClientService } from '../service/rest-client.service';
 import { UserService } from '../service/userService/user.service';
 
@@ -13,14 +16,17 @@ export class MyAccountComponent implements OnInit {
 
   user : User;
   email: string;
-  constructor(private userService:UserService,public checkAuthService: CheckAuthService) {
+  orders : Orders[];
+  constructor(private userService:UserService,public checkAuthService: CheckAuthService,public orderService:OrdersService) {
     this.user = new User("","","","",new Date(),"","", "","","","","",0,"","","");
     this.email="";
+    this.orders = new Array<Orders>();
   }
 
   ngOnInit() {
     this.email = this.checkAuthService.getToken();
     this.userService.getUser(this.email).subscribe(data => this.user = data,
       error=> JSON.parse(error).message);
+    this.orderService.getOrder(this.email).subscribe(data => this.orders = data);
   }
 }
