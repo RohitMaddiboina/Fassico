c32a180d7dbaee94d596d69afbaa294e707533df
Kranthi <settykranthi1@gmail.com>
2021-11-08 09:07:20 +0530

    After removing comments in wallet

--- src/app/wallet/wallet.component.ts
+++ src/app/wallet/wallet.component.ts
@@ -18,9 +18,6 @@ export class WalletComponent implements OnInit {
 
   userDetails: any=null
   orders: Orders[] = []
-  inActiveOrders: Orders[] = []
-  allOrders: Orders[] = []
-  paymentList:PaymentMethod[]=[]
   transaction:Transactions[]=[]
   paymentMethods=PaymentMethods
   transactionType=TransactionType
@@ -40,63 +37,16 @@ export class WalletComponent implements OnInit {
         }else if(d.transactionType==this.transactionType[0]){
           d.displaytransactionType=this.debit
         }
-        // if()
-        // d.paymentMethod=d.paymentMethod.valueOf(d.paymentMethod)
-        // if(PaymentMethods.PAY_ON_DELIVERY==d.paymentMethod){
-        //   d.paymentMethod="Pay On Delivery"
-        // }
       })
       this.transaction=data
-      console.log(data);
     })
-    // if(!this.checkAuth.isUserLoggedIn()){
-    //   this.router.navigate(['login'])
-    // }
     this.userDetails=this.user.getUserDetails(this.checkAuth.getToken()).subscribe(data => {
       this.userDetails=data
-      // console.log(this.userDetails)
       return data;
     })
     
-  this.paymentList=[{
-    'methodKey':'WALLET',
-    'methodValue':'Wallet'
-  },{
-    'methodKey':'PAY_ON_DELIVERY',
-    'methodValue':'Pay on Delivery'
-  }]
-
-
-    this.orderService.getOrder(this.checkAuth.getToken()).subscribe(data => {
-      data.map((d) => {
-        d.orderedDate=new Date(d.orderedDate.toString())
-        if(d.paymentMethod===this.paymentList[0].methodKey){
-          d.paymentMethod=this.paymentList[0].methodValue
-        }else{
-          d.paymentMethod=this.paymentList[1].methodValue
-        }        
-        if (!d.refundStatus)
-          d.orderCancellationDate = d.orderedDate
-          
-      })
-      this.orders = data
-      this.inActiveOrders = data.filter(d => d.refundStatus)
-      // this.allOrders = this.orders.concat(this.inActiveOrders).sort((a, b) => {
-
-      //   return Number(new Date(b.orderCancellationDate.toString())) - (Number)(new Date(a.orderCancellationDate.toString()));
-      // })
-      // console.log(this.allOrders)
-      return data
-    })
-    //  this.orders.sort((a,b){
-    //   return a.
-    //  })
   }
   
-}
-export interface PaymentMethod{
-  methodKey:String,
-  methodValue:String
 }
 enum PaymentMethods{
   'WALLET',
